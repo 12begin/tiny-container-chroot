80fc4a1a82143a5fde94144cc4c2e48518ccf05e
12begin <12begin@users.noreply.github.com>
2026-08-06 22:09:56 +0800

    修复 awk '{print $3}' 被转义的问题

--- app/src/main/java/com/fct/tc4/ui/page/ContainerMainViewModel.kt
+++ app/src/main/java/com/fct/tc4/ui/page/ContainerMainViewModel.kt
@@ -231,7 +231,7 @@ class ContainerMainViewModel(
             // 模仿 linuxdeploy 的 mount_part root 方式
             val bootScript = """
 # 0. 清理之前残留的挂载点，避免反复开关导致挂载点堆积
-mount | grep "$containerDir" | awk '{print \$3}' | sort -r | while read mp; do
+mount | grep "$containerDir" | awk '{print $3}' | sort -r | while read mp; do
     [ -n "${'$'}mp" ] && umount -l "${'$'}mp" 2>/dev/null
 done
 
